d4b953ed68e7aac9d5ea9ed6276ec089a2f3434f
Survi Sharma <survisharma1629@gmail.com>
2025-07-23 18:31:12 +0530

    Renamed file manually

--- TASK-4/CURRENCY_CONVERTER.java
+++ TASK-4/CURRENCY_CONVERTER.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class currency {
+public class CURRENCY_CONVERTER {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     
